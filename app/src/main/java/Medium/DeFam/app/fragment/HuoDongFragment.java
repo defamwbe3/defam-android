ab067a88e0bf230ebc476e78d7fd81935d9b0899
luozy <luozhiyong11@126.com>
2023-05-23 22:51:53 +0800

    代码提交

--- app/src/main/java/Medium/DeFam/app/fragment/HuoDongFragment.java
+++ app/src/main/java/Medium/DeFam/app/fragment/HuoDongFragment.java
@@ -182,8 +182,8 @@ public class HuoDongFragment extends BaseFragment {
             @Override
             public void onSuccess(JsonBean<BannerBean> data) {
                 banner.setAdapter(new ImageAdapter(getActivity(), data.getData().getData()))
-                        .setBannerGalleryMZ(20)
-                        .addPageTransformer(new ZoomOutPageTransformer())
+//                        .setBannerGalleryMZ(20)
+//                        .addPageTransformer(new ZoomOutPageTransformer())
                         .addBannerLifecycleObserver(getActivity())//添加生命周期观察者
                         .setIndicator(new CircleIndicator(getActivity()));
             }
