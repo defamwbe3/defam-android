00b9ed0e192eabbb6875254d1ae5ce1ed8a67a4f
luozy <luozhiyong11@126.com>
2023-06-09 22:46:24 +0800

    代码提交

--- app/src/main/java/Medium/DeFam/app/fragment/HomeFragment.java
+++ app/src/main/java/Medium/DeFam/app/fragment/HomeFragment.java
@@ -1,30 +1,21 @@
 package Medium.DeFam.app.fragment;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.util.TypedValue;
 import android.view.View;
-import android.view.animation.AccelerateInterpolator;
-import android.view.animation.DecelerateInterpolator;
 import android.widget.TextView;
-import android.widget.ViewFlipper;
 
-import androidx.fragment.app.Fragment;
-import androidx.viewpager.widget.ViewPager;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.hjq.toast.Toaster;
+import com.lzy.okgo.model.Response;
+import com.scwang.smart.refresh.layout.SmartRefreshLayout;
+import com.scwang.smart.refresh.layout.api.RefreshLayout;
+import com.scwang.smart.refresh.layout.listener.OnRefreshLoadMoreListener;
 import com.youth.banner.Banner;
 import com.youth.banner.indicator.CircleIndicator;
-
-import net.lucode.hackware.magicindicator.MagicIndicator;
-import net.lucode.hackware.magicindicator.ViewPagerHelper;
-import net.lucode.hackware.magicindicator.buildins.UIUtil;
-import net.lucode.hackware.magicindicator.buildins.commonnavigator.CommonNavigator;
-import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.CommonNavigatorAdapter;
-import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.IPagerIndicator;
-import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.IPagerTitleView;
-import net.lucode.hackware.magicindicator.buildins.commonnavigator.indicators.LinePagerIndicator;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -33,16 +24,16 @@ import java.util.Map;
 
 import Medium.DeFam.app.R;
 import Medium.DeFam.app.activity.BaoZang;
-import Medium.DeFam.app.activity.FaBu;
-import Medium.DeFam.app.activity.GongGao;
 import Medium.DeFam.app.activity.JiFenShop;
 import Medium.DeFam.app.activity.Search;
+import Medium.DeFam.app.activity.ZiXunDetail;
+import Medium.DeFam.app.adapter.HomeHotAdapter;
+import Medium.DeFam.app.adapter.HomeItemAdapter;
 import Medium.DeFam.app.adapter.ImageAdapter;
-import Medium.DeFam.app.adapter.ViewPagerAdapter;
 import Medium.DeFam.app.bean.BannerBean;
-import Medium.DeFam.app.bean.GongGaoBean;
 import Medium.DeFam.app.bean.InfoBean;
-import Medium.DeFam.app.common.ActivityRouter;
+import Medium.DeFam.app.bean.ZiXunBean;
+import Medium.DeFam.app.bean.ZiXunDetailBean;
 import Medium.DeFam.app.common.Constants;
 import Medium.DeFam.app.common.base.BaseFragment;
 import Medium.DeFam.app.common.bean.MessageEvent;
@@ -51,10 +42,9 @@ import Medium.DeFam.app.common.http.JsonBean;
 import Medium.DeFam.app.common.http.TradeHttpCallback;
 import Medium.DeFam.app.common.them.Eyes;
 import Medium.DeFam.app.utils.HttpUtil;
-import Medium.DeFam.app.view.ScaleTransitionPagerTitleView;
+import Medium.DeFam.app.view.recycle.RecyclerViewDivider;
 import butterknife.BindView;
 import butterknife.OnClick;
-import me.jessyan.autosize.utils.AutoSizeUtils;
 
 public class HomeFragment extends BaseFragment {
     public static HomeFragment newInstance(int type) {
@@ -65,24 +55,23 @@ public class HomeFragment extends BaseFragment {
         return fragment;
     }
 
-    @BindView(R.id.indicator)
-    MagicIndicator mIndicator;
-    @BindView(R.id.viewpager)
-    ViewPager viewPager;
     @BindView(R.id.banner)
     Banner banner;
-    @BindView(R.id.marquee_view)
-    ViewFlipper marqueeView;
     @BindView(R.id.integral)
     TextView integral;
     @BindView(R.id.suipian_total)
     TextView suipian_total;
 
-    ViewPagerAdapter viewPagerAdapter;
-    CommonNavigatorAdapter commonNavigatorAdapter;
-    List<String> titles = new ArrayList<>();
-    private ArrayList<Fragment> fragmentList = new ArrayList<>();
-    private int oldIndex = 0;
+    @BindView(R.id.refreshLayout)
+    SmartRefreshLayout refreshLayout;
+    @BindView(R.id.hot_rv)
+    RecyclerView hotRV;
+    HomeHotAdapter homeHotAdapter;
+    @BindView(R.id.recyclerView)
+    RecyclerView recyclerView;
+    HomeItemAdapter adapter;
+
+    private int page = 1;
 
     @Override
     protected boolean useEventBus() {
@@ -106,80 +95,68 @@ public class HomeFragment extends BaseFragment {
     @Override
     protected void initView(View view, Bundle savedInstanceState) {
         view.findViewById(R.id.topview).setPadding(0, Eyes.getStatusBarHeight(getActivity()), 0, 0);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
-        CommonNavigator commonNavigator = new CommonNavigator(getActivity());
-        commonNavigatorAdapter = new CommonNavigatorAdapter() {
-
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        layoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        RecyclerViewDivider rd = new RecyclerViewDivider(getActivity(), LinearLayoutManager.VERTICAL);
+        rd.setDividerHeight(0);
+        hotRV.addItemDecoration(rd);
+        hotRV.setLayoutManager(layoutManager);
+        homeHotAdapter = new HomeHotAdapter(getActivity());
+        hotRV.setAdapter(homeHotAdapter);
+        homeHotAdapter.setRecyclerViewOnItemClickListener(new HomeHotAdapter.OnRecyclerViewItemClickListener() {
             @Override
-            public int getCount() {
-                return titles == null ? 0 : titles.size();
+            public void onItemClick(int pos, int type, ZiXunDetailBean data) {
+                Intent intent = new Intent(getActivity(), ZiXunDetail.class);
+                intent.putExtra("data", data);
+                startActivity(intent);
             }
-
-            @Override
-            public IPagerTitleView getTitleView(Context context, final int index) {
-                ScaleTransitionPagerTitleView colorTransitionPagerTitleView = new ScaleTransitionPagerTitleView(context);
-                colorTransitionPagerTitleView.setNormalColor(getResources().getColor(R.color.color_666666));
-                colorTransitionPagerTitleView.setSelectedColor(getResources().getColor(R.color.color_1B1BDD));
-                colorTransitionPagerTitleView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 15);
-                //colorTransitionPagerTitleView.getPaint().setFakeBoldText(true);
-                colorTransitionPagerTitleView.setPadding(AutoSizeUtils.dp2px(getActivity(), 14), 0, AutoSizeUtils.dp2px(getActivity(), 14), 0);
-                colorTransitionPagerTitleView.setText(titles.get(index));
-                colorTransitionPagerTitleView.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        if (viewPager != null) {
-                            viewPager.setCurrentItem(index);
-                        }
-                    }
-                });
-                return colorTransitionPagerTitleView;
-            }
-
-            @Override
-            public IPagerIndicator getIndicator(Context context) {
-                LinePagerIndicator indicator = new LinePagerIndicator(context);
-                indicator.setMode(LinePagerIndicator.MODE_EXACTLY);
-                indicator.setLineHeight(UIUtil.dip2px(context, 2.5));
-                indicator.setLineWidth(UIUtil.dip2px(context, 24));
-                indicator.setRoundRadius(UIUtil.dip2px(context, 2));
-                indicator.setStartInterpolator(new AccelerateInterpolator());
-                indicator.setEndInterpolator(new DecelerateInterpolator(2.0f));
-                indicator.setColors(getResources().getColor(R.color.color_1B1BDD));
-
-                return indicator;
-            }
-        };
-        commonNavigator.setAdapter(commonNavigatorAdapter);
-        mIndicator.setNavigator(commonNavigator);
-
+        });
     }
 
     @Override
     public void initData() {
         super.initData();
-        useBanner();
-//        getGongGao();
+        adapter = new HomeItemAdapter(getContext());
+        adapter.setRecyclerViewOnItemClickListener(new HomeItemAdapter.OnRecyclerViewItemClickListener() {
+            @Override
+            public void onItemClick(int pos, int type, ZiXunDetailBean data) {
+                Intent intent = new Intent(getActivity(), ZiXunDetail.class);
+                intent.putExtra("data", data);
+                startActivity(intent);
+            }
+        });
+        recyclerView.setAdapter(adapter);
+        refreshLayout.setOnRefreshLoadMoreListener(new OnRefreshLoadMoreListener() {
+            @Override
+            public void onLoadMore(@NonNull RefreshLayout refreshLayout) {
+                getData();
+            }
 
-        viewPagerAdapter = new ViewPagerAdapter(getChildFragmentManager(), fragmentList);
-        //给ViewPager设置适配器
-        viewPager.setAdapter(viewPagerAdapter);
-        viewPager.addOnPageChangeListener(new MyOnPageChangeListener());//页面变化时的监听器
-        ViewPagerHelper.bind(mIndicator, viewPager);
+            @Override
+            public void onRefresh(@NonNull RefreshLayout refreshLayout) {
+                page = 1;
+                useBanner();
+                getData();
+            }
+        });
+        refreshLayout.autoRefresh();
+    }
 
-
-        titles.add("热门资讯");
-        fragmentList.add(HomeItemFragment.newInstance("", true));
-//        titles.add("热门活动");
-//        fragmentList.add(HomeItemHuoDongFragment.newInstance("hot"));
-        /*titles.add("热门KOL");
-        fragmentList.add(HomeItemKolFragment.newInstance(0));*/
-//        titles.add("热门文章");
-//        fragmentList.add(WenZhangFragment.newInstance(0));
-        viewPagerAdapter.notifyDataSetChanged();
-        commonNavigatorAdapter.notifyDataSetChanged();
-        viewPager.setOffscreenPageLimit(fragmentList.size());
-        viewPager.setCurrentItem(0);
-
+    @OnClick({R.id.sousuo, R.id.integral, R.id.suipian_total})
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.sousuo) {
+            Intent intent = new Intent(getActivity(), Search.class);
+            startActivity(intent);
+        }else if (id == R.id.integral) {
+            Intent intent = new Intent(getActivity(), JiFenShop.class);
+            startActivity(intent);
+        } else if (id == R.id.suipian_total) {
+            Intent intent = new Intent(getActivity(), BaoZang.class);
+            startActivity(intent);
+        }
     }
 
     private void useBanner() {
@@ -197,98 +174,48 @@ public class HomeFragment extends BaseFragment {
         });
     }
 
-    private void getGongGao() {
+    private void getData() {
         Map<String, String> map = new HashMap<>();
-        map.put("is_page", "0");
-        map.put("page", "1");
-        map.put("limit", "10");
-        HttpClient.getInstance().gets(HttpUtil.NOTICE, map, new TradeHttpCallback<JsonBean<List<GongGaoBean>>>() {
+        map.put("page", page + "");
+        HttpClient.getInstance().gets(HttpUtil.INDEXREALINFO, map, new TradeHttpCallback<JsonBean<ZiXunBean>>() {
             @Override
-            public void onSuccess(JsonBean<List<GongGaoBean>> data) {
-                if (null != data) {
-                    setMarqueeView(data.getData());
+            public void onSuccess(JsonBean<ZiXunBean> data) {
+                refreshLayout.finishRefresh();
+                refreshLayout.finishLoadMore();
+                if (null == data || null == data.getData()) {
+                    return;
                 }
+                if (page > 1) {
+                    refreshLayout.finishLoadMore();
+                    if (data.getData().getData().size() == 0) {
+                        Toaster.show("暂无更多数据");
+                        return;
+                    }
+                    adapter.addData(data.getData().getData());
+                } else {
+                    List<ZiXunDetailBean> list = data.getData().getData();
+                    //取前面4条记录当做精选
+                    if(list.size()>4){
+                        List<ZiXunDetailBean> hotList = new ArrayList<>();
+                        for(int i=0;i<4;i++){
+                            hotList.add(list.remove(0));
+                        }
+                        homeHotAdapter.replaceAll(hotList);
+                    }else{
+                        homeHotAdapter.replaceAll(list);
+                        list.clear();
+                    }
+                    adapter.replaceAll(list);
+                }
+                page++;
+            }
+
+            @Override
+            public void onError(Response<JsonBean<ZiXunBean>> response) {
+                super.onError(response);
+                refreshLayout.finishRefresh();
+                refreshLayout.finishLoadMore();
             }
         });
-    }
-
-    private void setMarqueeView(List<GongGaoBean> mNotifyList) {
-        marqueeView.removeAllViews();
-        for (int i = 0; i < mNotifyList.size(); i++) {
-            View view = View.inflate(getActivity(), R.layout.item_notify, null);
-            TextView notify = (TextView) view.findViewById(R.id.notify);
-            notify.setText(mNotifyList.get(i).getTitle());
-            marqueeView.addView(view);
-        }
-        if (mNotifyList.size() > 1) {
-            marqueeView.setAutoStart(true);
-            marqueeView.startFlipping();
-        } else {
-            marqueeView.setAutoStart(false);
-            marqueeView.stopFlipping();
-        }
-    }
-
-    private class MyOnPageChangeListener implements ViewPager.OnPageChangeListener {
-
-        @Override
-        public void onPageScrolled(int arg0, float arg1, int arg2) {
-            // TODO Auto-generated method stub
-
-        }
-
-        @Override
-        public void onPageScrollStateChanged(int arg0) {
-            // TODO Auto-generated method stub
-
-        }
-
-        @Override
-        public void onPageSelected(int position) {
-            if ((1 == position || 2 == position) && !isLogined()) {
-                ActivityRouter.toPoint(getActivity(), ActivityRouter.Mall.MALL_LOGIN);
-                new Handler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        viewPager.setCurrentItem(oldIndex);
-                    }
-                }, 200);  //延迟
-                return;
-            }
-            oldIndex = position;
-            if (fragmentList.get(position) instanceof HomeItemHuoDongFragment) {
-                HomeItemHuoDongFragment fragment = (HomeItemHuoDongFragment) fragmentList.get(position);//懒加载
-                if (null != fragment) {
-                    fragment.loadData();
-                }
-            } else if (fragmentList.get(position) instanceof WenZhangFragment) {
-                WenZhangFragment fragment = (WenZhangFragment) fragmentList.get(position);//懒加载
-                if (null != fragment) {
-                    fragment.loadData();
-                }
-            }
-
-        }
-    }
-
-    @OnClick({R.id.sousuo, R.id.gonggao, R.id.integral, R.id.suipian_total, R.id.img})
-    public void onClick(View v) {
-        int id = v.getId();
-        if (id == R.id.sousuo) {
-            Intent intent = new Intent(getActivity(), Search.class);
-            startActivity(intent);
-        } else if (id == R.id.gonggao) {
-            Intent intent = new Intent(getActivity(), GongGao.class);
-            startActivity(intent);
-        } else if (id == R.id.integral) {
-            Intent intent = new Intent(getActivity(), JiFenShop.class);
-            startActivity(intent);
-        } else if (id == R.id.suipian_total) {
-            Intent intent = new Intent(getActivity(), BaoZang.class);
-            startActivity(intent);
-        } else if (id == R.id.img) {
-            Intent intent = new Intent(getActivity(), FaBu.class);
-            startActivity(intent);
-        }
     }
 }
